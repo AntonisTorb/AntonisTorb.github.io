3a33f2b95b1e231d6dc6ace1316ea4c8b88a985a
antonistorb <antonistorb@yahoo.com>
2023-01-13 17:59:46 +0200

    Changes:
    - Added Languages section and some details on work experience.
    
    - Improved overall style of the page.
    
    - Improved page for mobile viewing by clearly displaying links without the need to hover.

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,24 @@
 let r = document.querySelector(':root');
-    let rs = getComputedStyle(r)
-    
-    function lightmode(){
-        r.style.setProperty("--mainTextColor", rs.getPropertyValue("--mainTextColor-light"));
-        r.style.setProperty("--secondaryTextColor", rs.getPropertyValue("--secondaryTextColor-light"));
-        r.style.setProperty("--mainLinkColor", rs.getPropertyValue("--mainLinkColor-light"));
-        r.style.setProperty("--mainBorderColor", rs.getPropertyValue("--mainBorderColor-light"));
-        r.style.setProperty("--mainBgColor", rs.getPropertyValue("--mainBgColor-light"));
-        r.style.setProperty("--secondaryBgColor", rs.getPropertyValue("--secondaryBgColor-light"));
-        document.getElementById("dark").style.setProperty("background-color", rs.getPropertyValue("--mainBgColor"))
-        document.getElementById("light").style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"))
-    }
+let rs = getComputedStyle(r)
 
-    function darkmode(){
-        r.style.setProperty("--mainTextColor", rs.getPropertyValue("--mainTextColor-dark"));
-        r.style.setProperty("--secondaryTextColor", rs.getPropertyValue("--secondaryTextColor-dark"));
-        r.style.setProperty("--mainLinkColor", rs.getPropertyValue("--mainLinkColor-dark"));
-        r.style.setProperty("--mainBorderColor", rs.getPropertyValue("--mainBorderColor-dark"));
-        r.style.setProperty("--mainBgColor", rs.getPropertyValue("--mainBgColor-dark"));
-        r.style.setProperty("--secondaryBgColor", rs.getPropertyValue("--secondaryBgColor-dark"));
-        document.getElementById("light").style.setProperty("background-color", rs.getPropertyValue("--mainBgColor"))
-        document.getElementById("dark").style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"))
-    }
+function lightmode(){
+    r.style.setProperty("--mainTextColor", rs.getPropertyValue("--mainTextColor-light"));
+    r.style.setProperty("--secondaryTextColor", rs.getPropertyValue("--secondaryTextColor-light"));
+    r.style.setProperty("--mainLinkColor", rs.getPropertyValue("--mainLinkColor-light"));
+    r.style.setProperty("--mainBorderColor", rs.getPropertyValue("--mainBorderColor-light"));
+    r.style.setProperty("--mainBgColor", rs.getPropertyValue("--mainBgColor-light"));
+    r.style.setProperty("--secondaryBgColor", rs.getPropertyValue("--secondaryBgColor-light"));
+    document.getElementById("dark").style.setProperty("background-color", rs.getPropertyValue("--mainBgColor"))
+    document.getElementById("light").style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"))
+}
+
+function darkmode(){
+    r.style.setProperty("--mainTextColor", rs.getPropertyValue("--mainTextColor-dark"));
+    r.style.setProperty("--secondaryTextColor", rs.getPropertyValue("--secondaryTextColor-dark"));
+    r.style.setProperty("--mainLinkColor", rs.getPropertyValue("--mainLinkColor-dark"));
+    r.style.setProperty("--mainBorderColor", rs.getPropertyValue("--mainBorderColor-dark"));
+    r.style.setProperty("--mainBgColor", rs.getPropertyValue("--mainBgColor-dark"));
+    r.style.setProperty("--secondaryBgColor", rs.getPropertyValue("--secondaryBgColor-dark"));
+    document.getElementById("light").style.setProperty("background-color", rs.getPropertyValue("--mainBgColor"))
+    document.getElementById("dark").style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"))
+}
