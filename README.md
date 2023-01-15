3c198618ce1f8431852e88850c31390c17379106
antonistorb <antonistorb@yahoo.com>
2023-01-15 21:52:58 +0200

    Changes:
    - Added Tabs at the top of the webpage for 'Main' and 'Projects' pages.
    
    - Stylized tabs and content.
    
    - Updated work history and CV attachment.
    
    - Updated JS code for tab changing.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### Hi there 👋
 My name is Antonios-Stefanos Tormpantonis and this is my personal website! It's only a static website, so it might be limited in functionality, but it suits my needs and my current experience with HTML and CSS.
 
+### Future
+- Populate Projects tab with a short description of each project, maybe some images.
+
 ### Inspirations:
 - [Demo Video by Traversy Media](https://www.youtube.com/watch?v=clwpf3VwCZQ&t=0s)
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,31 @@
 let r = document.querySelector(':root');
 let rs = getComputedStyle(r)
 
+function tabSwitch(evt, tab){
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none"
+    }
+
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+        tablinks[i].style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"));
+    }
+
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " active";
+    evt.currentTarget.style.setProperty("background-color", rs.getPropertyValue("--activeTabColor"));
+}
+
+function updateTabOnModeChange(){
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++){
+        tablinks[i].style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"));
+    }
+    document.getElementsByClassName("active")[0].style.setProperty("background-color", rs.getPropertyValue("--activeTabColor"));
+}
+
 function lightmode(){
     r.style.setProperty("--mainTextColor", rs.getPropertyValue("--mainTextColor-light"));
     r.style.setProperty("--secondaryTextColor", rs.getPropertyValue("--secondaryTextColor-light"));
@@ -10,6 +35,7 @@ function lightmode(){
     r.style.setProperty("--secondaryBgColor", rs.getPropertyValue("--secondaryBgColor-light"));
     document.getElementById("dark").style.setProperty("background-color", rs.getPropertyValue("--mainBgColor"))
     document.getElementById("light").style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"))
+    updateTabOnModeChange();
 }
 
 function darkmode(){
@@ -21,4 +47,9 @@ function darkmode(){
     r.style.setProperty("--secondaryBgColor", rs.getPropertyValue("--secondaryBgColor-dark"));
     document.getElementById("light").style.setProperty("background-color", rs.getPropertyValue("--mainBgColor"))
     document.getElementById("dark").style.setProperty("background-color", rs.getPropertyValue("--mainBorderColor"))
+    updateTabOnModeChange();
+}
+
+window.onload = function(){
+    document.getElementById("tab--main").click()
 }
